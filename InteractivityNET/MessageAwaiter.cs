dbecf600e88172297d47263d92d3e674d14700dc
Hinoki Shiba <3950465+gspalato@users.noreply.github.com>
2020-05-12 14:41:34 -0300

    Let it be!

---
+++ InteractivityNET/MessageAwaiter.cs
@@ -0,0 +1,47 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace InteractivityNET
+{
+	public class MessageAwaiter : EventAwaiter<IMessage>, IEventAwaiter<IMessage>
+	{
+		public readonly ITextChannel Channel;
+
+		public MessageAwaiter(DiscordSocketClient client,
+			ITextChannel channel, Predicate<IMessage> filter) : base(client, filter)
+		{
+			Channel = channel;
+		}
+
+		public override Task<IMessage> Wait(int millisecondsTimeout = 180000)
+		{
+			Client.MessageReceived += HandleEvent;
+
+			return base.Wait(millisecondsTimeout);
+		}
+
+		private Task HandleEvent(SocketMessage message)
+		{
+			if (Filter(message))
+			{
+				Tcs.SetResult(message);
+				Dispose();
+			}
+
+			return Task.CompletedTask;
+		}
+
+		public override void Dispose()
+		{
+			Client.MessageReceived -= HandleEvent;
+			base.Dispose();
+		}
+
+		~MessageAwaiter()
+		{
+			Dispose();
+		}
+	}
+}
---
+++ InteractivityNET/PaginatedContext.cs
@@ -0,0 +1,12 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace InteractivityNET
+{
+    public class PaginatedContext
+    {
+        public PaginatedMessage PaginatedMessage { get; set; }
+        public IUser User { get; set; }
+        public SocketReaction Reaction { get; set; }
+	}
+}
